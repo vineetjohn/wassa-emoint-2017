ba71c2ddae0967efd6f9beb74a3586ac623feba8
Vineet John <vinjohn10@gmail.com>
2017-04-24 11:10:57 -0400

    Added paragraph vector processor

--- bootstrap_doc2vec.py
+++ bootstrap_doc2vec.py
@@ -5,10 +5,10 @@ import sys
 import logging
 
 from data.options import Options
-from processors.s140_processor import S140Processor
+from processors.doc2vec_processor import Doc2VecProcessor
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     stream=sys.stdout,
     format='[%(asctime)s]: %(name)s : %(levelname)s : %(message)s'
 )
@@ -17,15 +17,13 @@ log = logging.getLogger(__name__)
 
 def main(argv):
     options = parse_args(argv)
-    processor = S140Processor(options)
+    processor = Doc2VecProcessor(options)
     processor.process()
 
 
 def parse_args(argv):
     parser = ArgumentParser(prog="wassa-task")
     parser.add_argument('--input_file_path', metavar='Input File Path',
-                        type=str, required=True)
-    parser.add_argument('--lexicon_file_path', metavar='Lexicon File Path',
                         type=str, required=True)
 
     return parser.parse_args(argv, namespace=Options())
--- processors/doc2vec_processor.py
+++ processors/doc2vec_processor.py
@@ -1,47 +1,42 @@
 import logging
 
+from gensim.models import Doc2Vec
 from sklearn import model_selection
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import LinearSVR
 
+from data.labeledline import LabeledLineSentence
 from processors.abstract_processor import Processor
 from utils import file_helper
 
 log = logging.getLogger(__name__)
 
 
-class S140Processor(Processor):
+class Doc2VecProcessor(Processor):
 
     def process(self):
-        log.info("S140Processor begun")
+        log.info("Doc2VecProcessor begun")
 
         input_tweets = file_helper.read_input_data(self.options.input_file_path)
-        s140_lexicon = list(file_helper.read_s140_lexicon(self.options.lexicon_file_path))
+        input_tweets_labeled = LabeledLineSentence(self.options.input_file_path)
+        model = Doc2Vec(input_tweets_labeled, iter=20, workers=8)
 
-        y_train = list()
         x_train = list()
-
+        y_train = list()
         count = 1
         for input_tweet in input_tweets:
             log.debug("On tweet " + str(count))
             count += 1
-            x_vector = list()
-            for lexicon_word in s140_lexicon:
-                if lexicon_word.word in input_tweet.text:
-                    x_vector.append(lexicon_word.score)
-                else:
-                    x_vector.append(0.0)
-            x_train.append(x_vector)
+            x_train.append(model.infer_vector(input_tweet.text.split()))
             y_train.append(input_tweet.intensity)
 
         log.debug("Computing model")
         scores = \
             model_selection.cross_val_score(
-                LinearSVR(), x_train, y_train, cv=10, scoring='r2'
+                LinearRegression(), x_train, y_train, cv=10, scoring='r2'
             )
         mean_score = scores.mean()
 
         log.info("Accuracy: %0.2f (+/- %0.2f)" % (mean_score, scores.std() * 2))
 
-        log.info("S140Processor ended")
+        log.info("Doc2VecProcessor ended")
