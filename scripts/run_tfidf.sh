1070648a8c6c34787be843ecfd804b63909a1366
Vineet John <vinjohn10@gmail.com>
2017-04-26 21:07:02 -0400

    Corrected train file path

--- scripts/run_tfidf.sh
+++ scripts/run_tfidf.sh
@@ -2,7 +2,7 @@
 
 CODEDIR=$(dirname "$0")"/../"
 
-INPUT_FILE_PATH="/home/v2john/MEGA/Academic/Masters/UWaterloo/Research/WASSA-Task/dataset/anger-all.txt"
+INPUT_FILE_PATH="/home/v2john/MEGA/Academic/Masters/UWaterloo/Research/WASSA-Task/dataset/anger-ratings-0to1.train.txt"
 
 /usr/bin/python3 "$CODEDIR"/bootstrap_tfidf.py \
 --input_file_path "$INPUT_FILE_PATH"
